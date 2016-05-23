3d6c027e4753dd7b06a12898e5a3c144accce392
Manassarn Manoonchai <manassarn@gmail.com>
2016-05-23 11:42:01 +0700

    Add Main component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
+import Main from './components/Main';
 
-render(<p>Hello, world</p>, document.getElementById('root'));
+render(<Main />, document.getElementById('root'));
